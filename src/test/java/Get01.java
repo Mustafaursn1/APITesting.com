6af320513528f462e29ac5cc6cf3fcdc4005aa8d
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2024-02-24 23:18:21 +0100

    first commit

---
+++ src/test/java/Get01.java
@@ -0,0 +1,62 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class Get01 {
+    /*
+    1.Postman , manuel Api testleri icin kullandik
+    2.Otomasyon tetsleri icn de Rest Assured Library kullanacagiz
+    3.Otomasyon testleri icin su adimlari izliyoruz
+       a)Gereksinimleri anlamak
+       b)Test case yaziyoruz
+         i)Test Case yaziminda "Gherkin" dilinini kullanacagiz
+         Gherkin dilinde kullancagimiz keywordler;
+            -Given:On kosullar
+            -When:Yapilacak aksiyonlar icin(get(), post() patch() ve delete() )
+            -Then:Istek yaptiktan sonra (request gönderdikten sonra )dogrulama icn
+            -And:Coklu islemlerde kullanacagiz.
+
+       c)Tets kodlarimizi yazmaya Baslayacagiz
+
+         i) set the URL
+         ii) set the expected Data (beklenen datanin olusturulmasi -->post(),put(),patch())
+         iii) Type code to send request(Talep gondermek icin kod yazimi)
+         iv)Do Assertion(dogrulama yapmak )
+
+
+
+Given https://restful-booker.herokuapp.com/booking/101
+When User sends a GET Request to the url
+Then HTTP Status Code should be 200
+And Content Type should be JSON
+And Status Line should be HTTP/1.1 200 0K
+
+
+
+     */
+
+    @Test
+    public void get01(){
+        //i) set the URL
+        String url="https://restful-booker.herokuapp.com/booking/101";
+        //ii) set the expected Data (beklenen datanin olusturulmasi -->post(),put(),patch())
+        //bizden suan bu case de bunlari istemdeigi icin bu meth kullanmiyoruz
+
+        //
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        //iv)Do Assertion(dogrulama yapmak )
+        response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+
+
+
+
+
+    }
+
+
+
+}
