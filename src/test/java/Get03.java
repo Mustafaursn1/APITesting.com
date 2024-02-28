bbcebea9b5ebda532b9a9b16a1b99e6b81f0bc33
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2024-02-28 16:48:12 +0100

    add spec

--- src/test/java/Get03.java
+++ src/test/java/Get03.java
@@ -1,7 +1,9 @@
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Get03 extends JsonplaceholderBaseUrl {
 
@@ -18,16 +20,31 @@ And "userId" is 2
     @Test
     public void get01() {
         //set The URL
-        spec.pathParams("first","todos","second",23);
+        spec.pathParams("first", "todos", "second", 23);
 
-        //Expected DATA-->suan yok
+        //set Expected DATA-->suan yok(put,patch, post)
 
         //Send teh request and Get Response
-        Response response=given().spec(spec).when().get("/{first}/{second}");
+        Response response = given().spec(spec).when().get("/{first}/{second}");
         response.prettyPrint();
 
-
-
+        //Do Assrertion  Then HTTP Status Code should be 200
+        //1.yol Hard Assert
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json").
+                body("title", equalTo("et itaque necessitatibus maxime molestiae qui quas velit")).
+                body("completed", equalTo(false)).
+                body("userId", equalTo(2));
+        System.out.println("*************************");
+        //2.yol  softAssert-->sadece body icinde gecerli
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"),
+                        "completed",equalTo(false),"userId",equalTo(2));
 
 
 
