337494de3e9330215e4fbf375109d9d9473b31ca
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2024-03-10 13:15:03 +0100

    add get Request with ObjectMapper

--- src/test/java/get_request/Get14ObjectMapper.java
+++ src/test/java/get_request/Get14ObjectMapper.java
@@ -1,7 +1,15 @@
 package get_request;
 
 import base_url.JsonPlaceholderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import testData.JsonPlaceholderTestData;
+import utilities.ObjectMapperUtils;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
 
 public class Get14ObjectMapper extends JsonPlaceholderBaseUrl {
 
@@ -24,21 +32,33 @@ public class Get14ObjectMapper extends JsonPlaceholderBaseUrl {
      */
 
     @Test
-    public void get01(){
+    public void get01() {
         //Send the url
-        spec.pathParams("first","todos","second",198);
+        spec.pathParams("first", "todos", "second", 198);
 
         //2 set the Expected Data and get the Response
+       String expectedData= new JsonPlaceholderTestData().expectedDataInString(10,"quis eius est sint explicabo",true);
+        Map<String,Object> expectedDataMap=ObjectMapperUtils.convertJsonToJava(expectedData, Map.class);
+        System.out.println("expectedDataMap: "+expectedDataMap);
 
 
         //3 send the get request and Get the Response
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
 
         //4 DO Assertions
+        Map<String,Object> actualData=ObjectMapperUtils.convertJsonToJava(response.asString(),Map.class);
+        System.out.println("actualData: "+actualData);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedDataMap.get("userId"),actualData.get("userId"));
+
+        assertEquals(expectedDataMap.get("title"),actualData.get("title"));
+        assertEquals(expectedDataMap.get("completed"),actualData.get("completed"));
 
 
     }
-
-
 
 
 }
