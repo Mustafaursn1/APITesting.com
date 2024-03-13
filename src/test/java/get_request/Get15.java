09e3b24cabc6c29758728abf7dc0317febd92e2b
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2024-03-13 23:15:34 +0100

    add get request with the Pojo&Objectmapper

--- src/test/java/get_request/Get15.java
+++ src/test/java/get_request/Get15.java
@@ -1,42 +1,63 @@
 package get_request;
 
 import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import pojo.BookingDatesPojo;
+import pojo.BookingPojo;
+import utilities.ObjectMapperUtils;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 public class Get15 extends RestfulBaseUrl {
     /*
     Given
-        https://restful-booker.herokuaoo.com/booking/22
+        https://restful-booker.herokuapp.com/booking/1385
     When
         I send Get Request to the URL
     Then
         Status code is 200
-        {
-        "firstname": "Guoqiang",
-        "lastname":"Morante Briones",
-        "totalprice": 111,
-        "depositpaid": true,
-        "bookingdates" :{
-                       "checkin":"2018-01-01"
-                       "checkout":"2019-01-01"
-                        },
-         "additionalneeds": "Breakfast"
-         }
+     {
+    "firstname": "Josh",
+    "lastname": "Allen",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
+    },
+    "additionalneeds": "super bowls"
+}
          */
 
     @Test
-    public void get01(){
+    public void get01() {
         //1 set the URL
-        spec.pathParams("first","booking","second",22);
+        spec.pathParams("first", "booking", "second", 1385);
 
         //2 set the expected Data
+        BookingDatesPojo bookingDatesPojo = new BookingDatesPojo("2018-01-01", "2019-01-01");
+        BookingPojo expectedData = new BookingPojo("Josh", "Allen", 111, true, bookingDatesPojo, "super bowls");
 
 
         //send The Get request and GET the response
-
-
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
 
         //Do Assertions
+        BookingPojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), BookingPojo.class);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getFirstname(), actualData.getFirstname());
+        assertEquals(expectedData.getLastname(), actualData.getLastname());
+        assertEquals(expectedData.getTotalprice(), actualData.getTotalprice());
+        assertEquals(expectedData.getDepositpaid(), actualData.getDepositpaid());
+        assertEquals(bookingDatesPojo.getCheckin(), actualData.getBookingdates().getCheckin());
+        assertEquals(bookingDatesPojo.getCheckout(), actualData.getBookingdates().getCheckout());
+
+        assertEquals(expectedData.getAdditionalneeds(), actualData.getAdditionalneeds());
+
 
     }
 
--- src/test/java/practice/Question1.java
+++ src/test/java/practice/Question1.java
@@ -33,27 +33,48 @@ otomasyon kodu yaziniz.
 
 
     @Test
-    public void post01(){
+    public void post01() {
         //1 set The url
-        spec.pathParams("first","user");
-        PetRestTestData obj=new PetRestTestData();
+        spec.pathParams("first", "user");
+        PetRestTestData obj = new PetRestTestData();
 
         //set the expected Data
-        String expectedData=obj.expectedDataInString(2,"Tim","John","Ceremy",
-                "abcd_jkl@gmail.com","234erdf_jgh","0978596886",1);
-        Map<String,Object> expectedDataMap=
-                ObjectMapperUtils.convertJsonToJava(expectedData,Map.class);
-        System.out.println("expectedData: "+expectedDataMap);
+        String expectedData = obj.expectedDataInString(2, "Tim", "John", "Ceremy",
+                "abcd_jkl@gmail.com", "234erdf_jgh", "0978596886", 1);
+        Map<String, Object> expectedDataMap =
+                ObjectMapperUtils.convertJsonToJava(expectedData, Map.class);
+        System.out.println("expectedData: " + expectedDataMap);
 
         //Send the Post Request and Get the Response
-        Response response=given().spec(spec).contentType("application/json").body(expectedDataMap).post("/{first}");
-        response.prettyPrint();
+        Response response1 = given().spec(spec).contentType("application/json").body(expectedDataMap).post("/{first}");
+        response1.prettyPrint();
 
-        System.out.println("statusCode:"+response.statusCode());
+        System.out.println("statusCode:" + response1.statusCode());
 
 
 
     }
+
+    @Test
+    public void get01(){
+        //set the url
+        spec.pathParams("first", "user", "second", "Tim");
+
+        //set the expected DATA
+
+        //send the get request and get the responsse
+
+        Response response2 = given().spec(spec).when().get("/{first}/{second}");
+        response2.prettyPrint();
+
+
+
+
+
+    }
+
+
+
 
 
 
